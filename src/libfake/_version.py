87f9f87902fef9a5d8ee03a886891f7ff17f1c62
GitHub Action <action@github.com>
2025-07-04 23:23:39 +0000

    🔖 Bump version to 2.0.0

--- src/libfake/_version.py
+++ src/libfake/_version.py
@@ -13,9 +13,9 @@ __description__ = (
 __url__ = "https://github.com/Pymmdrza/libfake"
 
 # Version components for programmatic access
-VERSION_MAJOR = 1
+VERSION_MAJOR = 2
 VERSION_MINOR = 0
-VERSION_PATCH = 1
+VERSION_PATCH = 0
 VERSION_SUFFIX = ""  # e.g., "a1", "b1", "rc1", "" for stable
 
 # Build the version string
