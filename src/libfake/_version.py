3a955a591ec124f84ad08e4ff137476ffcfa6350
MMDRZA <info@mmdrza.com>
2025-07-05 02:04:20 +0300

    Update _version.py

--- src/libfake/_version.py
+++ src/libfake/_version.py
@@ -15,7 +15,7 @@ __url__ = "https://github.com/Pymmdrza/libfake"
 # Version components for programmatic access
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
-VERSION_PATCH = 2
+VERSION_PATCH = 1
 VERSION_SUFFIX = ""  # e.g., "a1", "b1", "rc1", "" for stable
 
 # Build the version string
@@ -27,3 +27,4 @@ else:
 # Additional metadata
 __license__ = "MIT"
 __copyright__ = "2025 Mmdrza"
+
